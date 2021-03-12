542ffcb230f87dce61030d03a20f311535b4f4e4
Rohan <rohanjnr44@gmail.com>
2021-03-12 22:47:20 +0530

    Add and use asyncpg utlity functions.

--- bot/exts/fun/off_topic_names.py
+++ bot/exts/fun/off_topic_names.py
@@ -6,6 +6,7 @@ from typing import List
 from asyncpg import Record
 from bot import constants
 from bot.converters import OffTopicName
+from bot.postgres.utils import db_execute, db_fetch
 from discord.ext.commands import Bot, Cog, Context, group
 from loguru import logger
 
@@ -26,31 +27,30 @@ class OffTopicNames(Cog):
     @off_topic_names.command(name="add", aliases=("a",))
     async def add_ot_name(self, ctx: Context, name: OffTopicName) -> None:
         """Add off topic channel name."""
-        async with self.bot.db_pool.acquire() as connection:
-            async with connection.transaction():
-                await connection.execute(
-                    "INSERT INTO off_topic_channel_name(channel_name) VALUES($1)", name
-                )
+        await db_execute(
+            self.bot.db_pool,
+            "INSERT INTO off_topic_channel_name(channel_name) VALUES($1)",
+            name,
+        )
         await ctx.send(f"`{name}` has been added!")
 
     @off_topic_names.command(name="del", aliases=("remove", "delete"))
     async def delete_ot_name(self, ctx: Context, name: OffTopicName) -> None:
         """Delete off topic channel name."""
-        async with self.bot.db_pool.acquire() as connection:
-            async with connection.transaction():
-                await connection.execute(
-                    "DELETE FROM off_topic_channel_name WHERE channel_name=$1", name
-                )
+        await db_execute(
+            self.bot.db_pool,
+            "DELETE FROM off_topic_channel_name WHERE channel_name=$1",
+            name,
+        )
         await ctx.send(f"`{name}` has been deleted!")
 
     async def random_ot_name(self, limit: int = 1) -> List[Record]:
         """Fetch random ot channel name."""
-        async with self.bot.db_pool.acquire() as connection:
-            async with connection.transaction():
-                ot_names = await connection.fetch(
-                    "SELECT channel_name FROM off_topic_channel_name ORDER BY random() limit $1",
-                    limit,
-                )
+        ot_names = await db_fetch(
+            self.bot.db_pool,
+            "SELECT channel_name FROM off_topic_channel_name ORDER BY random() limit $1",
+            limit,
+        )
         return ot_names
 
     async def update_ot_channel_name(self) -> None:
@@ -61,11 +61,10 @@ class OffTopicNames(Cog):
                 now.date() + timedelta(days=1), time(0)
             )
             till_midnight = int((midnight_datetime - now).total_seconds())
-            # await asyncio.sleep(till_midnight)
             logger.info(
                 f"Sleeping for {till_midnight} before renaming off topic channel."
             )
-            await asyncio.sleep(60)
+            await asyncio.sleep(till_midnight)
 
             guild = self.bot.get_guild(constants.GUILD_ID)
             channel = guild.get_channel(constants.Channels.off_topic)
